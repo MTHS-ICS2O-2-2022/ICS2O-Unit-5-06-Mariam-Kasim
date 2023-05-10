36e276c9f90a4ade5cea30a06b8b162b03460ddd
Mariam-Kasim <124389357+Mariam-Kasim@users.noreply.github.com>
2023-05-10 17:09:09 +0000

    main

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Mariam Kasim
+// Created on: Mar 2023
 // This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function multiplies using only addition and loops
+ */
+function myButtonClicked() {
+
+  // Input
+  const number1 = parseInt(document.getElementById('number1').value)
+  const number2 = parseInt(document.getElementById('number2').value)
+
+  // Process
+  let counter = 0
+  let answer = 0
+  while (counter < number2) {
+    answer += number1
+    counter ++
+  }
+
+  // Output
+  document.getElementById('answer').innerHTML = number1 + ' x ' + number2 + ' = ' + answer
+}
